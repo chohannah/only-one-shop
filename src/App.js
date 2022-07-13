662170522dcd2d71dc1c85b051a325fc894a8aa0
Jinnie Cho <yejincho1105@gmail.com>
2022-07-13 18:27:36 +0900

    🎨 Implement Home route to App.js

--- src/App.js
+++ src/App.js
@@ -1,22 +1,12 @@
-import Directory from "./components/directory/directory.components";
+import { Routes, Route } from "react-router-dom";
+import Home from "./routes/home/home.component";
 
 const App = () => {
-  const categories = [
-    {
-      id: 1,
-      title: "Clothing",
-      image:
-        "https://cdn.shopify.com/s/files/1/0567/9690/5677/files/D32A0921_1080x.jpg?v=1633391513",
-    },
-    {
-      id: 2,
-      title: "Accessories",
-      image:
-        "https://cdn.shopify.com/s/files/1/0567/9690/5677/files/D32A0977_2_1_1080x.png?v=1633392297",
-    },
-  ];
-
-  return <Directory categories={categories} />;
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+    </Routes>
+  );
 };
 
 export default App;
---
+++ src/routes/home/home.component.jsx
@@ -0,0 +1,22 @@
+import Directory from "../../components/directory/directory.components";
+
+const Home = () => {
+  const categories = [
+    {
+      id: 1,
+      title: "Clothing",
+      image:
+        "https://cdn.shopify.com/s/files/1/0567/9690/5677/files/D32A0921_1080x.jpg?v=1633391513",
+    },
+    {
+      id: 2,
+      title: "Accessories",
+      image:
+        "https://cdn.shopify.com/s/files/1/0567/9690/5677/files/D32A0977_2_1_1080x.png?v=1633392297",
+    },
+  ];
+
+  return <Directory categories={categories} />;
+};
+
+export default Home;
