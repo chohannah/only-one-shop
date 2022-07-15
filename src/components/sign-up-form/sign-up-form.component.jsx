9b3b0d2e0ff2f43ecdbc0d383b61fbf676d1aa62
Jinnie Cho <yejincho1105@gmail.com>
2022-07-16 02:25:22 +0900

    ✨ Created and implemented setState in SignUpForm component

---
+++ src/components/sign-up-form/sign-up-form.component.jsx
@@ -0,0 +1,69 @@
+import { useState } from "react";
+
+const defaultFormFields = {
+  nickname: "",
+  email: "",
+  password: "",
+  confirmPassword: "",
+};
+
+const SignUpForm = () => {
+  const [formFields, setFormFields] = useState(defaultFormFields);
+  const { nickname, email, password, confirmPassword } = formFields;
+
+  console.log(formFields);
+
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+
+    setFormFields({ ...formFields, [name]: value });
+  };
+
+  return (
+    <div>
+      <h1>type your email and password</h1>
+
+      <form onSubmit={() => {}}>
+        <label>Nickname</label>
+        <input
+          type="text"
+          required
+          onChange={handleChange}
+          name="nickname"
+          value={nickname}
+        />
+
+        <label>Email</label>
+        <input
+          type="email"
+          required
+          onChange={handleChange}
+          name="email"
+          value={email}
+        />
+
+        <label>Password</label>
+        <input
+          type="password"
+          required
+          onChange={handleChange}
+          name="password"
+          value={password}
+        />
+
+        <label>Confirm Password</label>
+        <input
+          type="password"
+          required
+          onChange={handleChange}
+          name="confirmPassword"
+          value={confirmPassword}
+        />
+
+        <button type="submit">Sign up</button>
+      </form>
+    </div>
+  );
+};
+
+export default SignUpForm;
