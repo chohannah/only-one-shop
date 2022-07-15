96e631e5f6a1e3f09490bf48a7a4b7f001d2d720
Jinnie Cho <yejincho1105@gmail.com>
2022-07-15 23:55:33 +0900

    ⏪ Reverted SignIn component without Redirect-relevant scripts

--- src/routes/sign-in/sign-in.component.jsx
+++ src/routes/sign-in/sign-in.component.jsx
@@ -1,23 +1,9 @@
-// import { useEffect } from "react";
-// import { getRedirectResult } from "firebase/auth";
-
 import {
-  // auth,
   signInWithGooglePopup,
-  // signInWithGoogleRedirect,
   createUserDocumentFromAuth,
 } from "../../utils/firebase/firebase.utils";
 
 const SignIn = () => {
-  // useEffect(() => {
-  //   (async () => {
-  //     const response = await getRedirectResult(auth);
-  //     if (response) {
-  //       const userDocRef = await createUserDocumentFromAuth(response.user);
-  //     }
-  //   })();
-  // }, []);
-
   const logGoogleUser = async () => {
     const { user } = await signInWithGooglePopup();
     const userDocRef = await createUserDocumentFromAuth(user);
@@ -27,9 +13,6 @@ const SignIn = () => {
     <div>
       <h1>this is sign in page</h1>
       <button onClick={logGoogleUser}>Sign in with Google popup</button>
-      {/* <button onClick={signInWithGoogleRedirect}> 
-        Sign in with Google redirect
-      </button>*/}
     </div>
   );
 };
