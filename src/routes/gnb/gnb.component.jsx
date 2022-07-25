39673b9d3ef417ec155ba15a394561f2937e2eab
Jinnie Cho <yejincho1105@gmail.com>
2022-07-26 03:33:18 +0900

    ⚰️ Remove header className from GNB component

--- src/routes/gnb/gnb.component.jsx
+++ src/routes/gnb/gnb.component.jsx
@@ -8,43 +8,42 @@ import { ReactComponent as MenuIcon } from "../../assets/icon-menu.svg";
 const GNB = () => {
   return (
     <Fragment>
-      <header className="header">
-        <div className="gnb">
-          <h1 className="gnb-left">
-            <Link className="logo-wrapper" to="/">
-              <OpensLogo className="logo" />
-            </Link>
-          </h1>
+      <header className="gnb">
+        <h1 className="gnb-left">
+          <Link className="logo-wrapper" to="/">
+            <OpensLogo className="logo" />
+          </Link>
+        </h1>
 
-          <nav className="gnb-nav sm-hidden">
-            <h2 className="visually-hidden">menu</h2>
+        <nav className="gnb-nav sm-hidden">
+          <h2 className="visually-hidden">menu</h2>
 
-            <ul className="nav-list">
-              <Link className="nav-list-item" to="/all">
-                All
-              </Link>
-              <Link className="nav-list-item" to="/clothing">
-                Clothing
-              </Link>
-              <Link className="nav-list-item" to="/accessories">
-                Accessories
-              </Link>
-            </ul>
-          </nav>
+          <ul className="nav-list">
+            <Link className="nav-list-item" to="/all">
+              All
+            </Link>
+            <Link className="nav-list-item" to="/clothing">
+              Clothing
+            </Link>
+            <Link className="nav-list-item" to="/accessories">
+              Accessories
+            </Link>
+          </ul>
+        </nav>
 
-          <div className="gnb-right">
-            <Link className="user" to="/login">
-              <UserIcon className="icon-user" />
-            </Link>
-            <Link className="cart" to="/cart">
-              <CartIcon className="icon-cart" />
-            </Link>
-            <Link className="menu sm-only" to="/menu">
-              <MenuIcon className="icon-menu" />
-            </Link>
-          </div>
+        <div className="gnb-right">
+          <Link className="user" to="/login">
+            <UserIcon className="icon-user" />
+          </Link>
+          <Link className="cart" to="/cart">
+            <CartIcon className="icon-cart" />
+          </Link>
+          <Link className="menu sm-only" to="/menu">
+            <MenuIcon className="icon-menu" />
+          </Link>
         </div>
       </header>
+
       <Outlet />
     </Fragment>
   );
