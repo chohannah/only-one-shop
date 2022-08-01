38d4a7ab3d3f96b5c52124a5e994178a1c67e7a9
Jinnie Cho <yejincho1105@gmail.com>
2022-08-02 02:36:51 +0900

    🔨 Move createUserDocument from LogInForm to user.context.jsx

--- src/components/log-in-form/log-in-form.component.jsx
+++ src/components/log-in-form/log-in-form.component.jsx
@@ -1,11 +1,9 @@
 import { Link } from "react-router-dom";
 
-import { useState, useContext } from "react";
+import { useState } from "react";
 
 import FormInput from "../form-input/form-input.component";
 import Button from "../button/button.component";
-
-import { UserContext } from "../../contexts/user.context";
 
 import {
   signInWithGooglePopup,
@@ -21,15 +19,12 @@ const LogInForm = () => {
   const [formFields, setFormFields] = useState(defaultFormFields);
   const { email, password } = formFields;
 
-  const { setCurrentUser } = useContext(UserContext);
-
   const resetFormFields = () => {
     setFormFields(defaultFormFields);
   };
 
   const signInWithGoogle = async () => {
-    const { user } = await signInWithGooglePopup();
-    await signInWithGooglePopup(user);
+    await signInWithGooglePopup();
   };
 
   const handleSubmit = async (event) => {
@@ -37,8 +32,6 @@ const LogInForm = () => {
 
     try {
       const { user } = await signInAuthWithEmailAndPassword(email, password);
-      setCurrentUser(user);
-
       resetFormFields();
     } catch (error) {
       switch (error.code) {
