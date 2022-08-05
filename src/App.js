21aaa798e74b03092c2908618d663b2c6a298402
Jinnie Cho <yejincho1105@gmail.com>
2022-08-06 04:52:00 +0900

    ✨ Implement routes to App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import { Routes, Route } from "react-router-dom";
 import Home from "./routes/home/home.component";
+import GNB from "./routes/gnb/gnb.component";
+import SignUp from "./routes/sign-up/sign-up.component";
+import LogIn from "./routes/log-in/log-in.component";
+import Shop from "./routes/shop/shop.component";
 
 const App = () => {
   return (
     <Routes>
-      <Route path="/" element={<Home />} />
+      <Route path="/" element={<GNB />}>
+        <Route index element={<Home />}></Route>
+        <Route path="log-in" element={<LogIn />}></Route>
+        <Route path="sign-up" element={<SignUp />}></Route>
+        <Route path="shop" element={<Shop />}></Route>
+      </Route>
     </Routes>
   );
 };
