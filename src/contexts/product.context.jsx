6c81ae22d8cc8c31c3e29f77826a9f58e8c2a1b5
Jinnie Cho <yejincho1105@gmail.com>
2022-08-06 04:50:32 +0900

    ✨ Implement product context

---
+++ src/contexts/product.context.jsx
@@ -0,0 +1,13 @@
+import { createContext, useState } from "react";
+
+import PRODUCTS from "../products-data.json";
+
+export const ProductContext = createContext({});
+
+export const ProductProvider = ({ children }) => {
+  const [product, setProduct] = useState(PRODUCTS);
+  const value = { product };
+  return (
+    <ProductContext.Provider value={value}>{children}</ProductContext.Provider>
+  );
+};
