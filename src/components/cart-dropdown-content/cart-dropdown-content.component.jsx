91441e9c0e1e063a374874b16f8e54b07bd612d3
Jinnie Cho <yejincho1105@gmail.com>
2022-09-07 03:45:23 +0900

    🚚 Redirect CartDropdownItem in CartDropdownContent component

--- src/components/cart-dropdown-content/cart-dropdown-content.component.jsx
+++ src/components/cart-dropdown-content/cart-dropdown-content.component.jsx
@@ -4,7 +4,7 @@ import { useNavigate } from "react-router-dom";
 import { CartContext } from "../../contexts/cart.context";
 
 import Button from "../../components/button/button.component";
-import CartItem from "../../components/cart-item/cart-item.component";
+import CartItem from "../cart-dropdown-item/cart-dropdown-item.component";
 
 import { ReactComponent as CloseIcon } from "../../assets/icon-close.svg";
 
