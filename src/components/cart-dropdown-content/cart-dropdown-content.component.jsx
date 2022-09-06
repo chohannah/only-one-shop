0b783d3a144ad996dfb7f29c7d9dad3e078b599d
Jinnie Cho <yejincho1105@gmail.com>
2022-09-07 03:32:38 +0900

    🚚 Move CartDropdownContent component from routes folder to components folder

--- src/components/cart-dropdown-content/cart-dropdown-content.component.jsx
+++ src/components/cart-dropdown-content/cart-dropdown-content.component.jsx
@@ -1,12 +1,12 @@
 import { useContext } from "react";
 import { useNavigate } from "react-router-dom";
 
-import { CartContext } from "../../../contexts/cart.context";
+import { CartContext } from "../../contexts/cart.context";
 
-import Button from "../../../components/button/button.component";
-import CartItem from "../../../components/cart-item/cart-item.component";
+import Button from "../../components/button/button.component";
+import CartItem from "../../components/cart-item/cart-item.component";
 
-import { ReactComponent as CloseIcon } from "../../../assets/icon-close.svg";
+import { ReactComponent as CloseIcon } from "../../assets/icon-close.svg";
 
 const CartDropdownContent = () => {
   const { cartItems } = useContext(CartContext);
