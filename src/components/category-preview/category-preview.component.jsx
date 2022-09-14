392a434b886cfb644ea473bd4d83b9ebd2cd3b92
Jinnie Cho <yejincho1105@gmail.com>
2022-09-15 03:32:07 +0900

    🚚 Rename ProductAll component to CategoryPreview component

--- src/components/category-preview/category-preview.component.jsx
+++ src/components/category-preview/category-preview.component.jsx
@@ -1,13 +1,15 @@
 import ProductCard from "../product-card/product-card.component";
 
-const ProductAll = ({ title, products }) => {
+const CategoryPreview = ({ title, products }) => {
   return (
     <section>
       <h2>{title}</h2>
 
-      <div className="products">
+      <div className="shop">
+        {" "}
+        {/* temp */}
         {products
-          .filter((_, index) => index < 4)
+          .filter((_, index) => index < 3)
           .map((product) => (
             <ProductCard key={product.id} product={product} />
           ))}
@@ -16,4 +18,4 @@ const ProductAll = ({ title, products }) => {
   );
 };
 
-export default ProductAll;
+export default CategoryPreview;
