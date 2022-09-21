1ee98357f0628c668f5b149bcecae8150c002822
Jinnie Cho <yejincho1105@gmail.com>
2022-09-22 06:42:43 +0900

    ✨ Implement Persist in Store and index.js

--- src/store/store.js
+++ src/store/store.js
@@ -1,4 +1,6 @@
 import { compose, legacy_createStore, applyMiddleware } from "redux";
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
 // import logger from "redux-logger";
 
 import { rootReducer } from "./root-reducer";
@@ -17,12 +19,22 @@ const loggerMiiddleware = (store) => (next) => (action) => {
   console.log("next state: ", store.getState());
 };
 
+const persistConfig = {
+  key: "root",
+  storage,
+  blacklist: ["user"],
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
 const middleWares = [loggerMiiddleware];
 
 const composedEnhancers = compose(applyMiddleware(...middleWares));
 
 export const store = legacy_createStore(
-  rootReducer,
+  persistedReducer,
   undefined,
   composedEnhancers
 );
+
+export const persistor = persistStore(store);
