0d12212f51b1754955106cf5d84b8d849c70d693
Jinnie Cho <yejincho1105@gmail.com>
2022-09-22 06:52:21 +0900

    ⚡️ Implelemtn condition of middleware visibility in !production

--- src/store/store.js
+++ src/store/store.js
@@ -14,7 +14,9 @@ const persistConfig = {
 
 const persistedReducer = persistReducer(persistConfig, rootReducer);
 
-const middleWares = [logger];
+const middleWares = [process.env.NODE_ENV !== "production" && logger].filter(
+  Boolean
+);
 
 const composedEnhancers = compose(applyMiddleware(...middleWares));
 
