ca455095b05ae2497f7da52ddbeceb9c847fce4f
Jinnie Cho <yejincho1105@gmail.com>
2022-10-01 00:50:12 +0900

    🔨 Implement Spinner with isLoading Selector in Category and CategoriesPreview component

--- src/routes/category/category.component.jsx
+++ src/routes/category/category.component.jsx
@@ -1,14 +1,20 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, Fragment } from "react";
 import { useParams } from "react-router-dom";
 import { useSelector } from "react-redux";
 
-import { selectCategoriesMap } from "../../store/categories/category.selector";
+import {
+  selectIsLoading,
+  selectCategoriesMap,
+} from "../../store/categories/category.selector";
 
 import ProductCard from "../../components/product-card/product-card.component";
+import Spinner from "../../components/spinner/spinner.component";
+// import { createLogger } from "redux-logger";
 
 const Category = () => {
   const { category } = useParams();
   const categoriesMap = useSelector(selectCategoriesMap);
+  const isLoading = useSelector(selectIsLoading);
   const [products, setProducts] = useState(categoriesMap[category]);
 
   useEffect(() => {
@@ -16,12 +22,18 @@ const Category = () => {
   }, [category, categoriesMap]);
 
   return (
-    <div className="category">
-      {products &&
-        products.map((product) => (
-          <ProductCard key={product.id} product={product} />
-        ))}
-    </div>
+    <Fragment>
+      {isLoading ? (
+        <Spinner />
+      ) : (
+        <div className="category">
+          {products &&
+            products.map((product) => (
+              <ProductCard key={product.id} product={product} />
+            ))}
+        </div>
+      )}
+    </Fragment>
   );
 };
 
