b0dc307add588ae80c08cf8accd2720817d8f168
Jinnie Cho <yejincho1105@gmail.com>
2022-09-30 23:21:49 +0900

    🔨 Implement composeEnhancer in Store

--- src/store/store.js
+++ src/store/store.js
@@ -18,7 +18,13 @@ const middleWares = [process.env.NODE_ENV !== "production" && logger].filter(
   Boolean
 );
 
-const composedEnhancers = compose(applyMiddleware(...middleWares));
+const composeEnhancer =
+  (process.env.NODE_ENV !== "production" &&
+    window &&
+    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) ||
+  compose;
+
+const composedEnhancers = composeEnhancer(applyMiddleware(...middleWares));
 
 export const store = legacy_createStore(
   persistedReducer,
