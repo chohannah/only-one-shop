e9417b92f4b772f8783dee7e4d1e92b0bf481056
Jinnie Cho <yejincho1105@gmail.com>
2022-10-02 18:28:51 +0900

    🚚 Rename categoriesSaga to categoriesSagas

--- src/store/root-saga.js
+++ src/store/root-saga.js
@@ -1,7 +1,7 @@
 import { all, call } from "redux-saga/effects";
 
-import { categoriesSaga } from "./categories/category.saga";
+import { categoriesSagas } from "./categories/category.saga";
 
 export function* rootSaga() {
-  yield all([call(categoriesSaga)]);
+  yield all([call(categoriesSagas)]);
 }
