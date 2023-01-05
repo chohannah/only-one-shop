5417dc6fb49d22d0a35a21f87fa26c3b019d9d57
Jinnie Cho <yejincho1105@gmail.com>
2023-01-05 22:36:03 +0900

    🏷️ Type window in Store

--- src/store/store.ts
+++ src/store/store.ts
@@ -11,6 +11,12 @@ import { rootReducer } from "./root-reducer";
 
 export type RootState = ReturnType<typeof rootReducer>;
 
+declare global {
+  interface Window {
+    __REDUX_DEVTOOLS_EXTENSION_COMPOSE__?: typeof compose;
+  }
+}
+
 const persistConfig = {
   key: "root",
   storage,
