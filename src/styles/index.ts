4c94bf2e045cb5d37a847909a7f12c7b3ae3a203
Jinnie Cho <yejincho1105@gmail.com>
2023-03-10 17:50:04 +0900

    💄 Add colors in style DefaultTheme index.ts

--- src/styles/index.ts
+++ src/styles/index.ts
@@ -29,10 +29,36 @@ const letterSpacings = {
   sm: "1px",
 };
 
+const colors = {
+  bodyText: "#212529",
+  productText: "#303030",
+  white: "#fff",
+  black: "#000",
+  red: "#DC4231",
+  coolGray: {
+    100: "#fafafa",
+    200: "#efefef",
+    300: "#e4e4e4",
+    400: "#dadada",
+    500: "#cfcfcf",
+    600: "#8f8f8f",
+    700: "#5a5a5a",
+    800: "#454545",
+    900: "#2c2c2c",
+  },
+  warmGray: {
+    100: "#f7f7f7",
+    200: "#fbfaf9",
+    300: "#f2f0ec",
+    400: "#eae6df",
+  },
+};
+
 export const theme: DefaultTheme = {
   breakpoints,
   fontFamily,
   fontSizes,
   lineHeights,
   letterSpacings,
+  colors,
 };
