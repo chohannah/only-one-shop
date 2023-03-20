4ada0d54a3d36ff000816109bf87aa8dac8f2707
Jinnie Cho <yejincho1105@gmail.com>
2023-03-20 23:09:45 +0900

    🔨 Create utility functions for styles: flexbox, inlineFlexbox, columnFlexbox

---
+++ src/styles/utils/flexbox.ts
@@ -0,0 +1,41 @@
+import { css } from 'styled-components'
+
+function convertFullName(value: string): string {
+  switch (value) {
+    case 'start':
+      return 'flex-start'
+    case 'end':
+      return 'flex-end'
+    case 'between':
+      return 'space-between'
+    case 'around':
+      return 'space-around'
+    default:
+      return value
+  }
+}
+
+export function flexbox(jc: string = 'center', ai: string = 'center') {
+  return css`
+    display: flex;
+    justify-content: ${convertFullName(jc)};
+    align-items: ${convertFullName(ai)};
+  `
+}
+
+export function inlineFlexbox(jc: string = 'center', ai: string = 'center') {
+  return css`
+    display: inline-flex;
+    justify-content: ${convertFullName(jc)};
+    align-items: ${convertFullName(ai)};
+  `
+}
+
+export function columnFlexbox(jc: string = 'center', ai: string = 'center') {
+  return css`
+    display: flex;
+    flex-direction: column;
+    justify-content: ${convertFullName(jc)};
+    align-items: ${convertFullName(ai)};
+  `
+}
--- src/styles/utils/index.ts
+++ src/styles/utils/index.ts
@@ -1,3 +1,4 @@
 export { visuallyHidden } from './a11y'
 export { media } from './media'
 export { stripPx } from './helpers'
+export { flexbox, inlineFlexbox, columnFlexbox } from './flexbox'
