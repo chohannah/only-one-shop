a811debdfccffe08caffe73df1d84710c29539c3
Jinnie Cho <yejincho1105@gmail.com>
2023-03-21 00:22:15 +0900

    🐛 Redefined TypographyScales type / add sm and xxl from fontSizes, lineHeights, letterSpacings / remove semicolons automatically by Prettier

--- src/types/styled.d.ts
+++ src/types/styled.d.ts
@@ -1,75 +1,86 @@
-import "styled-components";
+import 'styled-components'
 
-declare module "styled-components" {
-  export type TypographyScales = "xxs | xs | base | sm | md | lg | xl";
+declare module 'styled-components' {
+  export type TypographyScales =
+    | 'xxs'
+    | 'xxsWide'
+    | 'xs'
+    | 'base'
+    | 'baseWide'
+    | 'sm'
+    | 'md'
+    | 'lg'
+    | 'xl'
 
   export interface DefaultTheme {
     breakpoints: {
-      tablet: string;
-      desktop: string;
-    };
+      tablet: string
+      desktop: string
+    }
 
     fontFamily: {
-      main: string;
-    };
+      main: string
+    }
 
     fontSizes: {
-      xxs: string;
-      xs: string;
-      base: string;
-      sm: string;
-      md: string;
-      lg: string;
-      xl: string;
-    };
+      xxs: string
+      xxsWide: string
+      xs: string
+      base: string
+      baseWide: string
+      sm: string
+      md: string
+      lg: string
+      xl: string
+    }
 
     lineHeights: {
-      xxs: number;
-      xxsWide: number;
-      xs: number;
-      base: number;
-      baseWide: number;
-      md: number;
-      lg: number;
-      xl: number;
-      xxl: number;
-    };
+      xxs: number
+      xxsWide: number
+      xs: number
+      base: number
+      baseWide: number
+      sm: number
+      md: number
+      lg: number
+      xl: number
+    }
 
     letterSpacings: {
-      xxs: string;
-      xxsWide: string;
-      xs: string;
-      base: string;
-      baseWide: string;
-      md: string;
-      lg: string;
-      xl: string;
-      xxl: string;
-    };
+      xxs: string
+      xxsWide: string
+      xs: string
+      base: string
+      baseWide: string
+      sm: string
+      md: string
+      lg: string
+      xl: string
+    }
 
     colors: {
-      bodyText: string;
-      productText: string;
-      white: string;
-      black: string;
-      red: string;
+      bodyText: string
+      productText: string
+      white: string
+      black: string
+      red: string
       coolGray: {
-        100: string;
-        200: string;
-        300: string;
-        400: string;
-        500: string;
-        600: string;
-        700: string;
-        800: string;
-        900: string;
-      };
+        100: string
+        200: string
+        300: string
+        400: string
+        500: string
+        600: string
+        700: string
+        800: string
+        900: string
+      }
       warmGray: {
-        100: string;
-        200: string;
-        300: string;
-        400: string;
-      };
-    };
+        100: string
+        200: string
+        300: string
+        400: string
+      }
+    }
   }
 }
