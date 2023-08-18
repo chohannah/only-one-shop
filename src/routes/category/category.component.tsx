be5f1eb24c4a430eabd3deea25fa936f54449b6f
Jinnie Cho <yejincho1105@gmail.com>
2023-08-19 01:48:09 +0900

    🏷️ Type Category

---
+++ src/routes/category/category.component.tsx
@@ -0,0 +1,46 @@
+import { useState, useEffect, Fragment } from 'react'
+import { useParams } from 'react-router-dom'
+import { useSelector } from 'react-redux'
+
+import {
+  selectIsLoading,
+  selectCategoriesMap,
+} from '../../store/categories/category.selector'
+
+import ProductCard from '../../components/product-card/product-card.component'
+import Spinner from '../../components/spinner/spinner.component'
+// import { createLogger } from "redux-logger";
+
+type CategoryRouteParams = {
+  category: string
+}
+
+const Category = () => {
+  const { category } = useParams<
+    keyof CategoryRouteParams
+  >() as CategoryRouteParams
+  const categoriesMap = useSelector(selectCategoriesMap)
+  const isLoading = useSelector(selectIsLoading)
+  const [products, setProducts] = useState(categoriesMap[category])
+
+  useEffect(() => {
+    setProducts(categoriesMap[category])
+  }, [category, categoriesMap])
+
+  return (
+    <Fragment>
+      {isLoading ? (
+        <Spinner />
+      ) : (
+        <div className="category">
+          {products &&
+            products.map((product) => (
+              <ProductCard key={product.id} product={product} />
+            ))}
+        </div>
+      )}
+    </Fragment>
+  )
+}
+
+export default Category
