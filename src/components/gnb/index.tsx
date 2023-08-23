21b341fd0cbf4fd7585a2ab0e4337bb4096c5cf8
Jinnie Cho <yejincho1105@gmail.com>
2023-08-24 03:13:13 +0900

    ♻️ Modify GNB: rename as Gnb, implement styled-components and handle responsiveness

--- src/components/gnb/index.tsx
+++ src/components/gnb/index.tsx
@@ -1,76 +1,75 @@
 import { Fragment } from 'react'
 import { Outlet, Link } from 'react-router-dom'
 import { useSelector, useDispatch } from 'react-redux'
+import clsx from 'clsx'
 
 import { selectCurrentUser } from '../../store/user/user.selector'
 import { signOutStart } from '../../store/user/user.action'
-import { selectCartCount } from '../../store/cart/cart.selector'
 
-import GnbCartDropdown from '../../components/cart-dropdown/cart-dropdown.component'
+import { useResponsive } from '../../hooks/useResponsive'
+import GnbCartDropdown from '../cart-dropdown'
 
-import { CartIcon, MenuIcon, LogInIcon, LogOutIcon } from '../../assets/icons'
-import { Logo } from '../../assets/images'
+import { MenuIcon, LogInIcon, SignOutIcon } from '../../assets/icons'
+import { LogoLetter } from '../../assets/images'
+import { StyledGnb, StyledGnbIconButton } from './styles'
+import { Grid } from '..'
+import { GnbNav } from './nav'
 
-const GNB = () => {
+const Gnb = () => {
   const currentUser = useSelector(selectCurrentUser)
-  const cartCount = useSelector(selectCartCount)
   const dispatch = useDispatch()
 
   const signOutUser = () => dispatch(signOutStart())
 
+  const { isMobile } = useResponsive()
+
   return (
     <Fragment>
-      <header className="gnb">
-        <h1 className="gnb-left">
-          <Link className="logo-wrapper" to="/">
-            <Logo className="logo" />
-          </Link>
-        </h1>
-
-        <nav className="gnb-nav sm-hidden">
-          <h2 className="visually-hidden">menu</h2>
-
-          <ul className="nav-list">
-            <Link className="nav-list-item" to="/shop">
-              All
-            </Link>
-            <Link className="nav-list-item" to="/shop/clothing">
-              Clothing
-            </Link>
-            <Link className="nav-list-item" to="/shop/accessories">
-              Accessories
-            </Link>
-          </ul>
-        </nav>
-
-        <div className="gnb-right">
-          {currentUser ? (
-            <div className="logout" onClick={signOutUser}>
-              <LogOutIcon className="icon-logout" />
-              sign out
+      <StyledGnb className={clsx('gnb')}>
+        <Grid sm={4}>
+          <div className="gnb-wrapper">
+            <div className="gnb-left">
+              {isMobile ? (
+                <StyledGnbIconButton
+                  className="gnb-icon-button"
+                  type="button"
+                  aria-label="open menu button"
+                >
+                  <MenuIcon />
+                </StyledGnbIconButton>
+              ) : (
+                <GnbNav />
+              )}
             </div>
-          ) : (
-            <Link className="login" to="/log-in">
-              <LogInIcon className="icon-login" />
-              log in
-            </Link>
-          )}
 
-          <Link className="cart sm-only" to="/cart">
-            <CartIcon className="icon-cart" />
-            <span className="cart-count">{cartCount}</span>
-          </Link>
-          <GnbCartDropdown />
+            <h1 className="logo">
+              <Link to="/">
+                <LogoLetter />
+              </Link>
+            </h1>
 
-          <Link className="menu sm-only" to="/menu">
-            <MenuIcon className="icon-menu" />
-          </Link>
-        </div>
-      </header>
+            <div className="gnb-right">
+              {currentUser ? (
+                <StyledGnbIconButton className="signout" onClick={signOutUser}>
+                  <SignOutIcon />
+                  {isMobile ? null : 'Sign Out'}
+                </StyledGnbIconButton>
+              ) : (
+                <StyledGnbIconButton className="login" as={Link} to="/log-in">
+                  <LogInIcon />
+                  {isMobile ? null : 'Log In'}
+                </StyledGnbIconButton>
+              )}
+
+              <GnbCartDropdown />
+            </div>
+          </div>
+        </Grid>
+      </StyledGnb>
 
       <Outlet />
     </Fragment>
   )
 }
 
-export default GNB
+export default Gnb
