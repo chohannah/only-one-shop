a68a3cc6b0722a17862b25735f44b70c04d289b0
Jinnie Cho <yejincho1105@gmail.com>
2023-08-29 01:40:30 +0900

    ✨ Create SubNav in SidebarDetails and apply grid

--- src/components/gnb/sidebar-details.tsx
+++ src/components/gnb/sidebar-details.tsx
@@ -1,14 +1,28 @@
 import React from 'react'
-import { NavLink } from 'react-router-dom'
+import { Link, NavLink } from 'react-router-dom'
 import clsx from 'clsx'
 
 import { navMap } from '../nav-map'
 
+import { Grid } from '../grid'
 import {
+  SocialTwitter,
+  SocialInstagram,
+  SocialPinterest,
+} from '../../assets/icons'
+import { LogoCircle } from '../../assets/images'
+
+import {
+  StyledGnbIconButton,
   StyledGnbSidebarDetails,
   StyledMainNav,
   StyledMainNavList,
   StyledMainNavListItem,
+  StyledSidebarLogo,
+  StyledSubNav,
+  StyledSubNavList,
+  StyledSubNavListItem,
+  StyledSubNavSocialIconGroup,
 } from './styles'
 import AuthControlButton from './auth-control-button'
 
@@ -17,26 +31,59 @@ interface SidebarDetailsProps {
 }
 
 const SidebarDetails: React.FC<SidebarDetailsProps> = ({ isOpen }) => {
-  const menuList = Object.keys(navMap.menuName)
+  const mainMenuList = Object.keys(navMap.mainMenuName)
+  const subMenuList = Object.keys(navMap.subMenuName)
 
   return (
     <StyledGnbSidebarDetails
       className={clsx('sidebar-details')}
       isOpen={isOpen}
     >
-      <StyledMainNav>
-        <StyledMainNavList>
-          {menuList.map((menuItem) => (
-            <StyledMainNavListItem key={menuItem}>
-              <NavLink to={`/${menuItem}`}>
-                {Object.values(navMap.menuName[menuItem])}
-              </NavLink>
-            </StyledMainNavListItem>
-          ))}
+      <Grid sm={4}>
+        <StyledMainNav className={clsx('main-nav')}>
+          <StyledMainNavList>
+            {mainMenuList.map((mainMenuItem) => (
+              <StyledMainNavListItem key={mainMenuItem}>
+                <NavLink to={`/${mainMenuItem}`}>
+                  {Object.values(navMap.mainMenuName[mainMenuItem])}
+                </NavLink>
+              </StyledMainNavListItem>
+            ))}
 
-          <AuthControlButton />
-        </StyledMainNavList>
-      </StyledMainNav>
+            <StyledMainNavListItem>
+              <AuthControlButton />
+            </StyledMainNavListItem>
+          </StyledMainNavList>
+        </StyledMainNav>
+
+        <StyledSubNav className={clsx('sub-nav')}>
+          <StyledSubNavList>
+            {subMenuList.map((subMenuItem) => (
+              <StyledSubNavListItem key={subMenuItem}>
+                <NavLink to={`/${subMenuItem}`}>
+                  {Object.values(navMap.subMenuName[subMenuItem])}
+                </NavLink>
+              </StyledSubNavListItem>
+            ))}
+
+            <StyledSubNavSocialIconGroup>
+              <StyledGnbIconButton as={Link} to="/">
+                <SocialTwitter />
+              </StyledGnbIconButton>
+              <StyledGnbIconButton as={Link} to="/">
+                <SocialInstagram />
+              </StyledGnbIconButton>
+              <StyledGnbIconButton as={Link} to="/">
+                <SocialPinterest />
+              </StyledGnbIconButton>
+            </StyledSubNavSocialIconGroup>
+          </StyledSubNavList>
+        </StyledSubNav>
+
+        <StyledSidebarLogo className={clsx('circle-logo')}>
+          <LogoCircle />
+        </StyledSidebarLogo>
+      </Grid>
     </StyledGnbSidebarDetails>
   )
 }
