5a4863f403a122112d3e539fec7a73320125dbd2
Jinnie Cho <yejincho1105@gmail.com>
2023-08-30 23:00:34 +0900

    🚚 Move image assets to public path and group logo assets in src path

--- src/assets/images/index.tsx
+++
@@ -1,2 +0,0 @@
-export { ReactComponent as GoogleLogo } from './logo-google.svg'
-export { ReactComponent as Logo } from './logo.svg'
---
+++ src/assets/logos/index.tsx
@@ -0,0 +1,3 @@
+export { ReactComponent as GoogleLogo } from './logo-google.svg'
+export { ReactComponent as LogoLetter } from './logo-letter.svg'
+export { ReactComponent as LogoCircle } from './logo-circle.svg'
