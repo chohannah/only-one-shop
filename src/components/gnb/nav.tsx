6dd38afe54ad702e83405752ebe3a527690787c7
Jinnie Cho <yejincho1105@gmail.com>
2023-08-31 20:39:57 +0900

    💫 Handle mouseEnter and Leave status

--- src/components/gnb/nav.tsx
+++ src/components/gnb/nav.tsx
@@ -1,25 +1,62 @@
-import { NavLink } from 'react-router-dom'
+import { useState } from 'react'
+import clsx from 'clsx'
+
 import { navMap } from '../nav-map'
+import { useResponsive } from '../../hooks/useResponsive'
+
 import {
   StyledGnbNav,
   StyledGnbNavMenuList,
   StyledGnbNavMenuListItem,
 } from './styles'
-import clsx from 'clsx'
+
+import { NavDropdown } from './nav-dropdown'
 
 export const GnbNav = () => {
-  const menuList = Object.keys(navMap.menuName)
+  const menuList = Object.keys(navMap.mainMenuName)
+  const [openDropdownFor, setOpenDropdownFor] = useState<string | null>(null)
+  const { isDesktop } = useResponsive()
+
+  const handleMouseEnter = (menuItem: string) => {
+    setOpenDropdownFor(menuItem)
+  }
+
+  const handleMouseLeave = () => {
+    setOpenDropdownFor(null)
+  }
 
   return (
     <StyledGnbNav className={clsx('gnb-nav')}>
       <h2 className="visually-hidden">menu</h2>
 
       <StyledGnbNavMenuList>
-        {menuList.map((menuItem) => (
-          <StyledGnbNavMenuListItem key={menuItem}>
-            <NavLink to={`/${menuItem}`}>{menuItem.toUpperCase()}</NavLink>
-          </StyledGnbNavMenuListItem>
-        ))}
+        {menuList.map((menuItem) => {
+          const isActive = openDropdownFor === menuItem
+
+          return (
+            <StyledGnbNavMenuListItem
+              key={menuItem}
+              onMouseEnter={() => handleMouseEnter(menuItem)}
+              onMouseLeave={handleMouseLeave}
+              isActive={isActive}
+            >
+              {isDesktop ? (
+                <button className="item-button">
+                  {menuItem.toUpperCase()}
+                </button>
+              ) : null}
+
+              {isActive && (
+                <NavDropdown
+                  menuItem={menuItem as keyof typeof navMap.navMenu}
+                  isActive={isActive}
+                />
+              )}
+
+              {/* <NavDropdown menuItem={menuItem as keyof typeof navMap.navMenu} /> */}
+            </StyledGnbNavMenuListItem>
+          )
+        })}
       </StyledGnbNavMenuList>
     </StyledGnbNav>
   )
