603130f5135c5b66646eec441c14f21b5ad45f62
Jinnie Cho <yejincho1105@gmail.com>
2023-09-13 22:00:56 +0900

    🚨 Update automatic syntax fixes due to linter: category types

--- src/store/categories/category.types.ts
+++ src/store/categories/category.types.ts
@@ -1,53 +1,46 @@
 export enum CATEGORIES_ACTION_TYPES {
-  FETCH_CATEGORIES_START = "category/FETCH_CATEGORIES_START",
-  FETCH_CATEGORIES_SUCESS = "category/FETCH_CATEGORIES_SUCESS",
-  FETCH_CATEGORIES_FAILED = "category/FETCH_CATEGORIES_FAILED",
+  FETCH_CATEGORIES_START = 'category/FETCH_CATEGORIES_START',
+  FETCH_CATEGORIES_SUCESS = 'category/FETCH_CATEGORIES_SUCESS',
+  FETCH_CATEGORIES_FAILED = 'category/FETCH_CATEGORIES_FAILED',
 }
 export type CategoryItemSizeUnit = {
-  inch: string;
-};
+  inch: string
+}
 
 export type CategoryItemSizeInch = {
-  length: Array<string>;
-  chest: Array<string>;
-  sleeve: Array<string>;
-};
+  length: Array<string>
+  chest: Array<string>
+  sleeve: Array<string>
+}
 
 export type CategoryItemSize = {
-  type: Array<string>;
-  name: Array<string>;
-  inch: CategoryItemSizeInch[];
-  unit: CategoryItemSizeUnit[];
-};
-
-export type CategoryItemImage = {
-  img1: string;
-  img2: string;
-  img3?: string;
-  img4?: string;
-};
+  type: Array<string>
+  name: Array<string>
+  inch: CategoryItemSizeInch[]
+  unit: CategoryItemSizeUnit[]
+}
 
 export type CategoryItemDescription = {
-  desc1: string;
-  desc2?: string;
-};
+  desc1: string
+  desc2?: string
+}
 
 export type CategoryItem = {
-  id: number;
-  name: string;
-  price: number;
-  isOnSale: boolean;
-  images: CategoryItemImage[];
-  description: CategoryItemDescription[];
-  size: CategoryItemSize[];
-};
+  id: number
+  name: string
+  price: number
+  isOnSale: boolean
+  image: string
+  description: CategoryItemDescription[]
+  size: CategoryItemSize[]
+}
 
 export type Category = {
-  title: string;
-  imageUrl: string;
-  items: CategoryItem[];
-};
+  title: string
+  imageUrl: string
+  items: CategoryItem[]
+}
 
 export type CategoryMap = {
-  [key: string]: CategoryItem[];
-};
+  [key: string]: CategoryItem[]
+}
