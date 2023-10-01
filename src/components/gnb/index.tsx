da68538b1cde53b77c7189f3e86be76f415101dd
Jinnie Cho <yejincho1105@gmail.com>
2023-10-01 23:43:11 +0900

    🔨 Set intersection observer to Gnb for backgroundColor and logo visibility toggling

--- src/components/gnb/index.tsx
+++ src/components/gnb/index.tsx
@@ -1,4 +1,4 @@
-import { Fragment } from 'react'
+import { useState, useEffect } from 'react'
 import { Outlet, Link } from 'react-router-dom'
 import clsx from 'clsx'
 
@@ -7,26 +7,60 @@ import { CartDropdown } from '../cart-dropdown'
 
 import { LogoLetter } from '../../assets/logos'
 
-import { Grid } from '..'
-import { GnbNav } from '..'
-import { Sidebar } from '..'
-import { AuthControlButton } from '..'
+import { Grid, GnbNav, Sidebar, AuthControlButton } from '..'
 
 import { StyledGnb } from './styles'
+import { theme } from '../../styles'
 
 export const Gnb = () => {
   const { isDesktop } = useResponsive()
+  const [show, setShow] = useState(false)
+
+  useEffect(() => {
+    let timeout: NodeJS.Timeout
+
+    const observer = new IntersectionObserver(
+      ([entry]) => {
+        clearTimeout(timeout)
+        timeout = setTimeout(() => {
+          setShow(!entry.isIntersecting)
+        }, 10) // 100ms debouncing time
+      },
+      { threshold: 0.1 }
+    )
+
+    const directory = document.querySelector('.home-carousel')
+
+    if (directory) {
+      observer.observe(directory)
+    }
+
+    return () => {
+      clearTimeout(timeout)
+      observer.disconnect()
+    }
+  }, [])
 
   return (
-    <Fragment>
-      <StyledGnb className={clsx('gnb')}>
+    <>
+      <StyledGnb
+        className={clsx('gnb')}
+        style={{
+          backgroundColor: show ? `${theme.colors.white}` : 'transparent',
+        }}
+      >
         <Grid sm={4}>
           <div className="gnb-wrapper">
             <div className="gnb-left">
               {isDesktop ? <GnbNav /> : <Sidebar />}
             </div>
 
-            <h1 className="logo">
+            <h1
+              className="logo"
+              style={{
+                display: show ? 'flex' : 'none',
+              }}
+            >
               <Link to="/">
                 <LogoLetter />
               </Link>
@@ -42,6 +76,6 @@ export const Gnb = () => {
       </StyledGnb>
 
       <Outlet />
-    </Fragment>
+    </>
   )
 }
