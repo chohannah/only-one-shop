e0f4cf795612cfcca34242ec0163b86a41cabd37
Jinnie Cho <yejincho1105@gmail.com>
2023-10-07 23:23:58 +0900

    🐛 Handle overflow in Products page

--- src/pages/products/styles.ts
+++ src/pages/products/styles.ts
@@ -6,6 +6,7 @@ import { GNB_HEIGHT } from '../../components/gnb/styles'
 
 export const StyledProducts = styled.main`
   margin-top: ${GNB_HEIGHT};
+  overflow: hidden;
 `
 
 export const StyledProductsHeader = styled.div`
